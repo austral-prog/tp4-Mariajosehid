d5b2fd013e7503695698ca9ec88f1e53e78aa432
Mariajosehid <mjhidalgo@mail.austral.edu.ar>
2025-09-01 11:22:07 -0300

    Add files via upload

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,16 @@
 def leap_year():
-    print("TO DO")
+
+
+    if (año % 4 == 0) and ((año % 100 != 0) or (año % 400 == 0)):
+        return True
+    else:
+        return False
+
+año = int(input("Ingrese un año: "))
+
+if leap_year():
+    print(f"El año {año} es bisiesto")
+else:
+    print(f"El año {año} no es bisiesto")
+
+leap_year()
