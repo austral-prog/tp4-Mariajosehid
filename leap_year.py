cd778543b7b97de366f35635529a2118a368aee7
Mariajosehid <mjhidalgo@mail.austral.edu.ar>
2025-09-01 11:23:19 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,6 +1,5 @@
 def leap_year():
 
-
     if (año % 4 == 0) and ((año % 100 != 0) or (año % 400 == 0)):
         return True
     else:
@@ -12,5 +11,3 @@ if leap_year():
     print(f"El año {año} es bisiesto")
 else:
     print(f"El año {año} no es bisiesto")
-
-leap_year()
